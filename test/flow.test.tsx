1c8ff9795307f167bc96a5334ef5e07fd0466299
Ryan Seery <ryanpseery@gmail.com>
2020-11-06 18:30:06 -0500

    readme

--- test/flow.test.tsx
+++ test/flow.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { flow } from '../src';
 
-describe('it', () => {
+describe('flow.div', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<flow.div />, div);
